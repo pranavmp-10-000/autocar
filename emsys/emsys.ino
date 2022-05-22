58a05120843635d42d632abd06454bfaaf508695
Pranav MP <pranav.workacc@gmail.com>
2022-05-23 01:21:38 +0530

    motor values update

--- emsys/emsys.ino
+++ emsys/emsys.ino
@@ -1,16 +1,16 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(2, OUTPUT); //Forward Right P
-  pinMode(3, OUTPUT); //Forward Right N
+  pinMode(8, OUTPUT); //Forward Right P
+  pinMode(9, OUTPUT); //Forward Right N
 
-  pinMode(5, OUTPUT); //Forward Left P
-  pinMode(6, OUTPUT); //Forward Left N
+  pinMode(11, OUTPUT); //Forward Left P
+  pinMode(12, OUTPUT); //Forward Left N
 
-  pinMode(9, OUTPUT); //Back Right P
-  pinMode(10, OUTPUT); //Back Right N
+  // pinMode(5, OUTPUT); //Back Right P
+  // pinMode(6, OUTPUT); //Back Right N
 
-  pinMode(11, OUTPUT); //Back Right P
-  pinMode(12, OUTPUT); //Back Right N
+  // pinMode(2, OUTPUT); //Back Right P
+  // pinMode(3, OUTPUT); //Back Right N
   Serial.begin(9600);
 }
 
@@ -18,6 +18,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available()){
     int traffic_state = Serial.parseInt();
+    Serial.println(traffic_state);
     if(traffic_state==1){
       forward();
     }
@@ -29,25 +30,25 @@ void loop() {
 }
 
 void forward() {
-  analogWrite(5, 100); analogWrite(6, 0);
-  analogWrite(9, 200); analogWrite(10, 0);
+  analogWrite(11, 150); analogWrite(12, 0);
+  analogWrite(9, 150); analogWrite(8, 0);
 
-  analogWrite(2, 100); analogWrite(3, 0);
-  analogWrite(11, 200); analogWrite(12, 0);
+  // analogWrite(2, 100); analogWrite(3, 0);
+  // analogWrite(11, 100); analogWrite(12, 0);
 }
 
 void reverse() {
   analogWrite(5, 0); analogWrite(6, 100);
   analogWrite(9, 0); analogWrite(10, 100);
 
-  analogWrite(2, 0); analogWrite(3, 100);
-  analogWrite(11, 0); analogWrite(12, 100);
+  // analogWrite(2, 0); analogWrite(3, 100);
+  // analogWrite(11, 0); analogWrite(12, 100);
 }
 
 void stopm(){
-  analogWrite(5, 0); analogWrite(6, 0);
-  analogWrite(9, 0); analogWrite(10, 0);
-
-  analogWrite(2, 0); analogWrite(3, 0);
   analogWrite(11, 0); analogWrite(12, 0);
+  analogWrite(8, 0); analogWrite(9, 0);
+
+  // analogWrite(2, 0); analogWrite(3, 0);
+  // analogWrite(11, 0); analogWrite(12, 0);
 }
