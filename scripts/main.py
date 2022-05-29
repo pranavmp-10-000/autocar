8a8d140708784e0ca1d32e9bcc0fa17e6221b74a
Pranav MP <pranav.workacc@gmail.com>
2022-05-30 01:33:26 +0530

    bug fixes

--- scripts/main.py
+++ scripts/main.py
@@ -6,13 +6,12 @@ import serial
 
 
 def init_camera():
-    vid = cv2.VideoCapture(0)
+    vid = cv2.VideoCapture(-1)
     return vid
 
 
 def init_serial():
-    ser = serial.Serial('/dev/ttyACM0', 9600 ,timeout=.1)
-    #ser.baudrate = 115200
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
     ser.write('Serial Initialized'.encode('utf-8'))
     return ser
 
@@ -20,36 +19,61 @@ def init_serial():
 def main():
     cap = init_camera()
     ser = init_serial()
-    # logging.info(f'Camera Initialized: {cap.get(3)}')
+    print(f'Camera Initialized: {cap.get(3)}')
+    start_time = time.time()
     dl_engine = dl.DLObjectDetecction()
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f'Model Loaded in {elapsed_time}')
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+
+    size = (frame_width, frame_height)
+
+    i = 0
+    states = 0
     while(True):
         ret, frame = cap.read()
+
         if ret:
-            # cv2.imshow(f'Frame', frame)
-            el_t = dl_engine.run_inference(frame)
+            el_t, pred_img = dl_engine.run_inference(frame)
+            # cv2.imwrite(f'/home/pranav/autocar/scripts/imgs/{str(i)}.jpg',pred_img)
+            # time.sleep(.2)
             el_t = round(el_t, 3)
+            i = i+1
+            states = states + dl_engine.human_state
+            print(
+                f'Human State: {str(dl_engine.human_state)} @@@@@@ Traffic State: {str(dl_engine.traffic_state)} in {el_t}s')
+            # if i<10:
+            #     cv2.imwrite(f'/home/pranav/autocar/scripts/imgs/{str(i)}.jpg',pred_img)
+            # else:
+            #     i=0
             if dl_engine.human_state == 1:
-                ser.write(bytes('0','utf-8'))
+                ser.write(bytes('0', 'utf-8'))
                 ser.flush()
                 time.sleep(0.05)
-            else:
+                states = 0
+            elif(states == 0):
                 # if dl_engine.traffic_state == 1:
                 #     ser.write(bytes('2','utf-8'))
                 #     ser.flush()
                 #     time.sleep(0.05)
                 # else:
-                ser.write(bytes('2','utf-8'))
+                ser.write(bytes('1', 'utf-8'))
                 ser.flush()
                 time.sleep(0.05)
                 # data = ser.readline()
                 # print(data)
                 # time.sleep(0.1)
-            print(
-                f'Human State: {str(dl_engine.human_state)} @@@@@@ Traffic State: {str(dl_engine.traffic_state)} in {el_t}s')
-            
+            if states == 2:
+                states = 0
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        if KeyboardInterrupt:
+            break
     cap.release()
+    ser.close()
+    # result.release()
     cv2.destroyAllWindows()
 
 
